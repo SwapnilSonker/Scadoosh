4a160b016b3eea08a87257292cfb37270647d603
SwapnilSonker <swapnilsonker1432@gmail.com>
2024-03-05 22:30:56 +0530

    sidebar added

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './_components/navbar'
+import Sidebar from './_components/sidebar'
 
 const BrowserLayout = ({children,}: {children : React.ReactNode}) => {
   return (
     <>
     <Navbar/>
     <div className='flex h-full pt-20'>
+      <Sidebar/>
         {children}
     </div>
     </>
