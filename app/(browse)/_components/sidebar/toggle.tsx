f3a61e4a52c93fa8186c6c6351ac1fa62d8bd56d
SwapnilSonker <swapnilsonker1432@gmail.com>
2024-03-07 22:41:25 +0530

    hint collapsable added

--- app/(browse)/_components/sidebar/toggle.tsx
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Hint } from "@/components/hint";
 import { Button } from "@/components/ui/button";
 import { useSidebar } from "@/store/use-sidebar";
 import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react";
@@ -13,14 +14,17 @@ const Toggle = () => {
     <>
       {collapsed && (
         <div className="hidden lg:flex w-full items-center justify-center pt-4 mb-4">
+          <Hint label={label} side="right" asChild>
           <Button onClick={onExpand} className="h-auto p-2" variant="ghost">
             <ArrowRightFromLine className="h-4 w-4" />
           </Button>
+          </Hint>
         </div>
       )}
       {!collapsed && (
         <div className="p-3 pl-6 mb-2 flex items-center w-full">
           <p className="font-semibold text-primary">For you</p>
+          <Hint label={label} side="right" asChild>
           <Button
             onClick={onCollapse}
             className="h-auto p-2 ml-auto"
@@ -28,6 +32,7 @@ const Toggle = () => {
           >
             <ArrowLeftFromLine className="h-4 w-4" />
           </Button>
+        </Hint>
         </div>
       )}
     </>
