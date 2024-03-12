4027369b49be3aa9589847f8ee66342a5879918d
SwapnilSonker <swapnilsonker1432@gmail.com>
2024-03-12 22:37:40 +0530

    Hydration Errors fixed

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,22 +1,38 @@
-"use client"
+"use client";
 
 import { cn } from "@/lib/utils";
-import { useSidebar } from "@/store/use-sidebar"
+import { useSidebar } from "@/store/use-sidebar";
+import { useState, useEffect } from "react";
+import { ToggleSkeleton } from "./toggle";
+import { RecommendedSkeleton } from "./recommended";
+import { useIsClient } from "usehooks-ts";
 
-interface WrapperProps{
-children:React.ReactNode,
+interface WrapperProps {
+  children: React.ReactNode;
 }
-export const Wrapper = ({
-    children, 
-}: WrapperProps) => {
+export const Wrapper = ({ children }: WrapperProps) => {
+  //   const [isClient, setIsClient] = useState(false);
+  const isClient = useIsClient();
+  const { collapsed } = useSidebar((state) => state);
 
-    const { collapsed } = useSidebar((state) => state);
+  if (!isClient)
+    return (
+      <aside className="fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+        <ToggleSkeleton />
+        <RecommendedSkeleton />
+      </aside>
+    );
 
-    return <>
-    <aside className={cn(
-        "fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50",
-        collapsed && "w-[70px]")}>
+  return (
+    <>
+      <aside
+        className={cn(
+          "fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50",
+          collapsed && "w-[70px]"
+        )}
+      >
         {children}
-    </aside>
+      </aside>
     </>
-}
+  );
+};
