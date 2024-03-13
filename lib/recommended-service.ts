7844f40a260a9dc45c9b1f3f247ccd85602087bc
SwapnilSonker <swapnilsonker1432@gmail.com>
2024-03-13 23:03:27 +0530

    setting up follow services

--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -1,14 +1,45 @@
+import { getSelf } from "./auth-service";
 import { db } from "./db"
 
 export const getRecommended = async() => {
 
     //for checking the hydration error
     // await new Promise(resolve => setTimeout(resolve , 5000));
-    const users = await db.user.findMany({
-        orderBy: {
+
+
+    let userId;
+
+    try {
+        const self = await getSelf();
+        userId = self.id;
+        
+    } catch (error) {
+        userId = null;
+        
+    }
+
+    let users: any = [];
+    if(userId){
+       users = await db.user.findMany({
+        where:{
+            NOT:{
+                id: userId,
+            }
+        },
+        orderBy:{
             createdAt: "desc"
         }
-    });
+        })
+    }
+    else{
+        users = await db.user.findMany({
+            orderBy: {
+                createdAt: "desc"
+            }
+        });
+    }
+
+    
 
     return users;
 }
