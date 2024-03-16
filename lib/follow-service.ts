124ff7bb892936449367f5df149a27f95afbd332
SwapnilSonker <swapnilsonker1432@gmail.com>
2024-03-17 00:33:52 +0530

    following list is done

--- lib/follow-service.ts
+++ lib/follow-service.ts
@@ -1,6 +1,24 @@
 import { getSelf } from "./auth-service";
 import { db } from "./db";
 
+export const getFollowedUsers = async() =>{
+    try {
+        const self = await getSelf();
+
+        const followedUser = db.follow.findMany({
+            where:{
+                followerId: self.id,
+            },
+            include:{
+                following: true,
+            }
+        });
+
+        return followedUser;
+    } catch  {
+        return [];
+    }
+}
 export const isFollowingUser = async( id: string) => {
     try {
         const self = await getSelf();
