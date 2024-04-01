c8e504d5025196ffa63c5f458870c8116933ef3c
SwapnilSonker <swapnilsonker1432@gmail.com>
2024-04-01 22:25:04 +0530

    chat settings done

--- app/(dashboard)/u/[username]/chat/_components/toggle-card.tsx
+++ app/(dashboard)/u/[username]/chat/_components/toggle-card.tsx
@@ -1,6 +1,10 @@
 "use client"
 
 import { Switch } from "@/components/ui/switch";
+import {toast } from "sonner";
+import {useTransition} from "react";
+import { updateStream } from "@/actions/stream";
+import { Skeleton } from "@/components/ui/skeleton";
 
 type FieldTypes = "isChatEnabled" | "isChatDelayed" | "isChatFollowersOnly"
 
@@ -10,7 +14,14 @@ interface ToggleCardProps{
     field: FieldTypes;
 }
 const ToggleCard = ({label, value = false, field}: ToggleCardProps) => {
-
+    const [isPending , startTransition] = useTransition()
+    const onChange = () => {
+        startTransition(() => {
+            updateStream({ [field]: !value})
+            .then(() => toast.success("Chat settongs updated"))
+            .catch(() => toast.error("Something went wrong"))
+        })
+    }
   return (
     <div className="rounded-xl bg-muted p-6">
         <div className="flex items-center justify-between">
@@ -18,7 +29,10 @@ const ToggleCard = ({label, value = false, field}: ToggleCardProps) => {
                 {label}
             </p>
             <div className="space-y-2">
-                <Switch checked={value}>
+                <Switch
+                disabled={isPending}
+                onCheckedChange={onChange} 
+                checked={value}>
                     {value? "On" : "Off"}
                 </Switch>
             </div>
@@ -28,3 +42,9 @@ const ToggleCard = ({label, value = false, field}: ToggleCardProps) => {
 }
 
 export default ToggleCard
+
+export const ToggleCardSkeleton = () => {
+    return (
+        <Skeleton className="rounded-xl p-10 w-full"/>
+    )
+}
