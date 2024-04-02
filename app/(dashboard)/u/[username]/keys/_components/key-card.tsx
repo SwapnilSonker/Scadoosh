05e1f72db8bda66b25cf42d8455569120aff3a9c
SwapnilSonker <swapnilsonker1432@gmail.com>
2024-04-02 21:38:05 +0530

    consoles removed and formatting done

--- app/(dashboard)/u/[username]/keys/_components/key-card.tsx
+++ app/(dashboard)/u/[username]/keys/_components/key-card.tsx
@@ -1,43 +1,37 @@
-"use client"
+"use client";
 
 import { Input } from "@/components/ui/input";
 import CopyButton from "./copy-button";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 
-interface KeyCardProps{
-    value: string | null;
+interface KeyCardProps {
+  value: string | null;
 }
 
-
-const KeyCard = ({value}:KeyCardProps) => {
-    const [show, setShow] = useState(false);
+const KeyCard = ({ value }: KeyCardProps) => {
+  const [show, setShow] = useState(false);
   return (
     <div className="rounded-xl bg-muted p-6">
-        <div className="flex items-start gap-x-10">
-            <p className="font-semibold shrink-0">
-                Stream Key
-            </p>
-            <div className="space-y-2 w-full">
-                <div className="w-full items-center gap-x-2" >
-                    <Input 
-                    value={value || ""}
-                    type={show ? "text": "password"}
-                    disabled
-                    placeholder="Stream key"/>
-                    <CopyButton value={value || ""}/>
-                </div>
-                <Button 
-                onClick={() => setShow(!show)}
-                size="sm"
-                variant="link"
-                >
-                    {show ? "Hide" : "Show"}
-                </Button>
-            </div>
+      <div className="flex items-start gap-x-10">
+        <p className="font-semibold shrink-0">Stream Key</p>
+        <div className="space-y-2 w-full">
+          <div className="w-full items-center gap-x-2">
+            <Input
+              value={value || ""}
+              type={show ? "text" : "password"}
+              disabled
+              placeholder="Stream key"
+            />
+            <CopyButton value={value || ""} />
+          </div>
+          <Button onClick={() => setShow(!show)} size="sm" variant="link">
+            {show ? "Hide" : "Show"}
+          </Button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default KeyCard
+export default KeyCard;
