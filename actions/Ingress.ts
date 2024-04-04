db4182bf26826e6ca201a54f0cf464d132f89944
SwapnilSonker <swapnilsonker1432@gmail.com>
2024-04-04 22:33:04 +0530

    packages done

--- actions/Ingress.ts
+++ actions/Ingress.ts
@@ -1,1 +1,19 @@
-"use client"
+"use server"
+
+import{
+    IngressAudioEncodingPreset,
+    IngressClient, IngressInput,
+    IngressVideoEncodingPreset,
+    RoomServiceClient,
+    TrackSource
+} from "livekit-server-sdk"
+
+import { db } from "@/lib/db"
+import { getSelf } from "@/lib/auth-service"
+
+
+const roomService = new RoomServiceClient(
+    process.env.LIVEKIT_API_URL!,
+    process.env.LIVEKIT_API_KEY!,
+    process.env.LIVEKIT_API_SECRET!
+)
